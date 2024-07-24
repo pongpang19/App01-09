23e4cfa5e4f3887b05a3f4d9c194f05ea671feff
rung-arun thapphan <rungarun1182@gmail.com>
2024-07-24 14:46:45 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow,IonCol,IonLabel,IonButton,IonItem} 
+from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonGrid, IonRow,IonCol,IonLabel,IonButton,IonItem ],
 })
 export class HomePage {
   constructor() {}
